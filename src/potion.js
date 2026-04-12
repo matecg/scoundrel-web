332bf1ea5b94a87bf8b6781dcf0d2c6d1b042631
Gody <250132528+matecg@users.noreply.github.com>
2026-04-12 19:42:00 -0300

    add a class representing health potions

---
+++ src/potion.js
@@ -0,0 +1,23 @@
+import Card from "./card.js";
+import ScoundrelCard from "./scoundrelcard.js";
+
+export default class Potion extends ScoundrelCard {
+
+    /**
+     * Creates potions instances from Hearts suit cards.
+     * @param {Card} card - The deck card for this potion
+     */
+    constructor(card) {
+        if (card.suit.label !== "hearts")
+            throw new Error("Potions must be from Hearts suit");
+
+        super(card);
+    }
+
+    interact(player) {
+        if (this.interacted) return;
+        
+        player.heal(this.getValue());
+        this.interacted = true;
+    }
+}
