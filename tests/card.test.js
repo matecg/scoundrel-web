21ec62f6f3fbfbd86a0c152429c50482ef566809
Gody <250132528+matecg@users.noreply.github.com>
2026-04-14 06:56:27 -0300

    change local definition for ranks and suits to proper import

--- tests/card.test.js
+++ tests/card.test.js
@@ -1,28 +1,6 @@
 import { expect, test } from "@jest/globals";
 import Card from "../src/card.js";
-
-const SUITS = [
-    {label: "hearts", icon: "♥️"},
-    {label: "diamonds", icon: "♦️"}, 
-    {label: "spades", icon: "♠️"},
-    {label:  "clubs", icon: "♣️"}
-];
-
-const RANKS = [
-    {label: "two", icon: "2"}, 
-    {label: "three", icon: "3"}, 
-    {label: "four", icon: "4"}, 
-    {label: "five", icon: "5"}, 
-    {label: "six", icon: "6"}, 
-    {label: "seven", icon: "7"}, 
-    {label: "eight", icon: "8"},
-    {label: "nine", icon:  "9"},
-    {label: "ten", icon:  "10"},
-    {label: "valet", icon: "J"},
-    {label: "queen", icon: "Q"},
-    {label: "king", icon: "K"},
-    {label: "ace", icon: "A"}
-];
+import { SUITS, RANKS } from "../src/helpers.js";
 
 test('can create a card', () => {
     const card = new Card(SUITS[2], RANKS[2]);
--- tests/deck.test.js
+++ tests/deck.test.js
@@ -1,29 +1,7 @@
 import Card from "../src/card.js";
 import Deck from "../src/deck.js";
 import { expect, test } from "@jest/globals";
-
-const SUITS = [
-    {label: "hearts", icon: "♥️"},
-    {label: "diamonds", icon: "♦️"}, 
-    {label: "spades", icon: "♠️"},
-    {label:  "clubs", icon: "♣️"}
-];
-
-const RANKS = [
-    {label: "two", icon: "2"}, 
-    {label: "three", icon: "3"}, 
-    {label: "four", icon: "4"}, 
-    {label: "five", icon: "5"}, 
-    {label: "six", icon: "6"}, 
-    {label: "seven", icon: "7"}, 
-    {label: "eight", icon: "8"},
-    {label: "nine", icon:  "9"},
-    {label: "ten", icon:  "10"},
-    {label: "valet", icon: "J"},
-    {label: "queen", icon: "Q"},
-    {label: "king", icon: "K"},
-    {label: "ace", icon: "A"}
-];
+import { SUITS, RANKS } from "../src/helpers.js";
 
 test('can create standard deck of 52 cards', () => {
     const deck = new Deck();
