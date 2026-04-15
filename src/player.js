67cf3e32dbabe248014ca51c821e9a6d5f1dcd89
Gody <250132528+matecg@users.noreply.github.com>
2026-04-15 07:13:38 -0300

    change combat function to handle weapon errors

--- src/player.js
+++ src/player.js
@@ -7,14 +7,45 @@ export default class Player {
         this.canSkip = true;
     }
 
+    /**
+     * Regenerates the players health.
+     * @param {number} amount  - The amount to heal
+     */
     heal(amount) {
         if (typeof amount !== "number" || amount <= 0)
             throw new Error("Invalid healing amount provided");
         this.health = amount + this.health > 20 ? 20 : amount + this.health;
     }
 
+    /**
+     * Equips a new weapon, discarding the previous.
+     * @param {Weapon} weapon - The new weapon.
+     */
     equipWeapon(weapon) {
         if (!(weapon instanceof Weapon)) return;
         this.weapon = weapon;
+    }
+
+    /**
+     * Fight against a creature from the dungeon.
+     * @param {number} creaturePower - The creature's power level
+     */
+    combat(creaturePower) {
+        let damage = 0;
+        try {
+            if (this.weapon) {
+                damage = this.weapon.attack(creaturePower);
+                if (damage < 0) damage = 0;
+            }
+            else
+                damage = creaturePower;
+        } catch (error) {
+            damage = creaturePower;    
+        } finally {
+            this.health -= damage;    
+            if (this.health <= 0)
+                this.health = 0;
+                // dispatch an event?
+        }
     }
 }
