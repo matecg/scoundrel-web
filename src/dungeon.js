5309f7d33322090b691d35ab2ae71fd7c4138a31
Gody <250132528+matecg@users.noreply.github.com>
2026-04-28 20:41:32 -0300

    change dungeon.js to work with functional programming

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,73 +1,66 @@
-import Card from "./card.js";
-import Creature from "./creature.js";
-import Deck from "./deck.js";
-import { RANKS, SUITS } from "./helpers.js";
-import { REMOVE_POTS_AND_WEAPONS, ROOM_SIZE } from "./helpers.js";
-import Potion from "./potion.js";
-import Weapon from "./weapon.js";
-
-
-export default class Dungeon {
-    constructor() {
-        this.content = this.#createDungeon();
-        this.canSkip = true;
-        this.room = this.moveToNextRoom();
+/**
+ * Extract the current value and entity name from a card.
+ * @param {{suit: string, rank: string}} card - A card to transform
+ * @returns {{entity: string, value: number}}
+ */
+export function getEntityAndValue(card) {
+    const output = {}
+    switch (card.suit) {
+        case "♥️":
+            output.entity = "potion";
+            break;
+        case "♦️":
+            output.entity = "weapon";
+            break;
+        case "♣️":
+        case "♠️":
+            output.entity = "creature";
+            break;
+        default:
+            break;
     }
+    const rankIdx = DECK.ranks.findIndex(rank => rank === card.rank);
+    output.value = rankIdx + 2;
+    return output;
+}
 
-    moveToNextRoom(current = []) {
-        // Game about to start *ONLY*
-        if (this.#isEmpty(this.room) && this.#isEmpty(current)) {
-            this.room = this.content.draw(ROOM_SIZE);
+/**
+ * Generates the next room, based on the current room state.
+ * @param {Object} dg - A dungeon object
+ */
+export function getNextRoom(dg) {
+    if (dg.nextRoom.length == 0 || dg.nextRoom.length == 1) {
+        dg.canSkip = true;
+        dg.nextRoom = dg.elements.splice(0, 4);
+    } else if (dg.nextRoom.length == 4 && dg.canSkip) {
+        dg.canSkip = false;
+        dg.elements.push(...dg.nextRoom);
+        dg.nextRoom = dg.elements.splice(0, 4);
+    }
+}
+
+/**
+ * Creates a new dungeon object instance.
+ * @returns {Object}
+ */
+export function createDungeon() {
+    const elements = [];
+    for (let i = 0; i < DECK.suits.length; i++) {
+        for (let j = 0; j < DECK.ranks.length; j++) {
+            if ((DECK.suits[i] == "♥️" || DECK.suits[i] == "♦️")
+                && SKIP_FROM.includes(DECK.ranks[j])) continue;
+            elements.push({ suit: DECK.suits[i], rank: DECK.ranks[j] });
         }
-        if (current.length === ROOM_SIZE) {
-            if (this.canSkip && this.#isSameRoom(current)) {
-                this.canSkip = false;
-                this.content.push(this.room);
-                this.room = this.content.draw(ROOM_SIZE)
-            } else {
-                throw new Error("Invalid skip usage. A room cannot be skipped twice in a row.")
-            }
-        }
-        if (current.length === 1) {
-            this.canSkip = false ? true : this.canSkip;
-            this.room = current.concat(this.content.draw(ROOM_SIZE - 1));
-        }
-        this.room = this.room.map(card => {
-            switch (card.suit.label) {
-                case "spades":
-                case "clubs":
-                    return new Creature(card);
-                case "diamonds":
-                    return new Weapon(card);
-                case "hearts":
-                    return new Potion(card);
-                default:
-                    throw new Error("Invalid card received");
-            }
-        });
-        return this.room.slice();
     }
-
-    isFullyExplored() {
-        return this.room.filter(entity => entity.interacted).length === 3;
+    for (let i = 0; i < 1000; i++) {
+        const randomIdx = Math.floor(Math.random() * elements.length);
+        const fromIdx = i % elements.length;
+        [elements[randomIdx], elements[fromIdx]] = [elements[fromIdx], elements[randomIdx]]
     }
-
-    #createDungeon() {
-        const deck = new Deck();
-        const ranksToRemove = RANKS.filter(r => REMOVE_POTS_AND_WEAPONS.some(icon => r.icon === icon));
-        const hearts = ranksToRemove.map(rank => new Card(SUITS[0], rank));
-        const diamonds = ranksToRemove.map(rank => new Card(SUITS[1], rank));
-        const toRemove = hearts.concat(diamonds);
-        deck.remove(toRemove);
-        deck.shuffle();
-        return deck;
-    }
-
-    #isEmpty(room = []) {
-        return room.length === 0
-    }
-
-    #isSameRoom(target) {
-        return target.every(c => this.room.some(card => card.equal(c)))
+    return {
+        elements,
+        canSkip: true,
+        nextRoom: [],
+        interacted: []
     }
 }
