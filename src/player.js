20c17338c2c555f0c8042fe9441c479309fd1225
Gody <250132528+matecg@users.noreply.github.com>
2026-04-28 20:39:37 -0300

    change player to functional style

--- src/player.js
+++ src/player.js
@@ -1,54 +1,32 @@
-import { MAX_HEALTH } from "./helpers.js";
-import Weapon from "./weapon.js";
-
-export default class Player {
-    constructor(name) {
-        this.health = MAX_HEALTH;
-        this.weapon = null;
-        this.name = name;
-    }
-
-    /**
-     * Regenerates the players health.
-     * @param {number} amount  - The amount to heal
-     */
-    heal(amount) {
-        if (typeof amount !== "number" || amount <= 0)
-            throw new Error("Invalid healing amount provided");
-        this.health = amount + this.health > MAX_HEALTH ? MAX_HEALTH : amount + this.health;
-    }
-
-    /**
-     * Equips a new weapon, discarding the previous.
-     * @param {Weapon} weapon - The new weapon.
-     */
-    equipWeapon(weapon) {
-        if (!(weapon instanceof Weapon)) 
-            throw new Error("Weapon must be an instance from Weapon class");
-        this.weapon = weapon;
-    }
-
-    /**
-     * Fight against a creature from the dungeon.
-     * @param {number} creaturePower - The creature's power level
-     */
-    combat(creaturePower) {
-        let damage = 0;
-        try {
-            if (this.weapon) {
-                damage = this.weapon.attack(creaturePower);
-                if (damage < 0) damage = 0;
-            }
-            else
-                damage = creaturePower;
-        } catch (error) {
-            damage = creaturePower;    
-        } finally {
-            this.health -= damage;    
-            if (this.health <= 0)
-                this.health = 0;
-                // dispatch an event?
-            return damage;
+/**
+ * Creates a new player object.
+ * @param {string} name - Player's name
+ * @returns {Object}
+ */
+export function createPlayer(name) {
+    return {
+        name,
+        health: MAX_HEALTH,
+        weapon: {
+            value: 0,
+            durability: []
         }
     }
+}
+
+/**
+ * Tries to interact with a given index card from the current dungeon room.
+ * @param {Object} dg - A dungeon object
+ * @param {Object} player - A player object
+ * @param {number} index - The index to interact with
+ */
+export function interactWithEntity(dg, player, index) {
+    if (dg.interacted.includes(dg.nextRoom[index])) return;
+    if (index >= dg.nextRoom.length) return;
+    if (dg.nextRoom.length === 1) return;
+
+    const next = dg.nextRoom.splice(index, 1)[0];
+    dg.interacted.push(next);
+    const { entity, value } = getEntityAndValue(next);
+    ENTITIES[entity](player, value);
 }
